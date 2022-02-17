1217346e5b9bf9d09ed2f3c1edcb7e6f9763eb2b
PotatoHead007 <99846241+PotatoHead007@users.noreply.github.com>
2022-02-17 12:38:40 +1100

    testing

--- something.go
+++ something.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "os"
+    "regexp"
+
+    "github.com/urfave/cli/v2"
+)
 
 var modes = {
     "1: Basic Arithmetics",
@@ -10,33 +17,99 @@ var modes = {
 }
 
 func main() {
-    fmt.Println("Welcome to CLI-Calculator.")
-    fmt.Println("Please select a mode to continue:")
-    for _, mode = range modes {
-        fmt.Println(mode)
-    }
+    var app = &cli.App{
+        Name: "cli-calculator",
+        Description: "A CLI Calculator.",
+        Action: func(c *cli.Context) error {
+            fmt.Println("Welcome to CLI-Calculator.")
+            fmt.Println("Please select a mode to continue:")
+            for _, mode := range modes {
+                fmt.Println(mode)
+            }
 
-    var chosenMode int8
-    var _, err = fmt.Scan(&chosenMode)
-    if err != nil {
-        fmt.Println("Failiure to read from user input. Input may be of incorrect type.")
-        return
+            var chosenMode int8
+            var _, err = fmt.Scan(&chosenMode)
+            if err != nil {
+                fmt.Println("Failiure to read from user input. Input may be of incorrect type.")
+                return
+            }
+            switch chosenMode {
+            case 1:
+                fmt.Println("Basic Arithmetics selected.")
+                arithProcess()
+            case 2:
+                fmt.Println("Comparisons selected.")
+                compProcess()
+            case 3: 
+                fmt.Println("Fractions, Decimals, & Percentages selected.")
+                fdpProcess()
+            case 4:
+                fmt.Println("Trigonometry selected")
+                trigProcess()
+            default:
+                fmt.Printf("%v is not a valid choice of mode currently.")
+                return
+            }
+        },
+        Commands: []*cli.Command{
+			{
+				Name:    "arithmics",
+				Aliases: []string{"a", "1"},
+				Usage:   "Quickly compute arithmic problems without using the interactive mode.",
+				Action: func(c *cli.Context) error {
+					arithProcess(c.Args())
+					return nil
+				},
+			},
+			{
+				Name:    "comparisons",
+				Aliases: []string{"c", "2"},
+				Usage:   "Quickly compare values without using the interactive mode.",
+				Action: func(c *cli.Context) error {
+					compProcess(c.Args())
+					return nil
+				},
+			},
+			{
+				Name:    "fdp",
+				Aliases: []string{"2"},
+				Usage:   "Quickly compute fractions, decimals or percentages without using the interactive mode.",
+				Action: func(c *cli.Context) error {
+					fdpProcess(c.Args())
+					return nil
+				},
+			},
+			{
+				Name:    "trigonometry",
+				Aliases: []string{"t", "4"},
+				Usage:   "Quickly compute trigonometry-related problems without using the interactive mode.",
+				Action: func(c *cli.Context) error {
+					trigProcess(c.Args())
+					return nil
+				},
+			},
+		},
     }
-    switch chosenMode {
-    case 1:
-        fmt.Println("Basic Arithmetics selected.")
-        arithProcess()
-    case 2:
-        fmt.Println("Comparisons selected.")
-        compProcess()
-    case 3: 
-        fmt.Println("Fractions, Decimals, & Percentages selected.")
-        fdpProcess()
-    case 4:
-        fmt.Println("Trigonometry selected")
-        trigProcess()
-    default:
-        fmt.Printf("%v is not a valid choice of mode currently.")
-        return
+}
+
+func arithProcess(args cli.Args) {
+    arithRegexVerify = regexp.MustCompile(`(?<!.)\d+ ?(?:(?:[\+\-]|(?:[\*][\*]?))|(?:[\/][\/]?)) ?\d+(?: ?(?:(?:[\+\-]|(?:[\*][\*]?))|(?:[\/][\/]?)) ?\d+)*(?!.)`)
+    var matches = arithRegexVerify.FindString(args[0])
+    
+    fmt.Println("something")
+    for idx, match := range matches {
+        fmt.Printf("%v == %s\n", idx, match)
     }
+}
+
+func compProcess(args cli.Args) {
+
+} 
+
+func fdpProcess(args cli.Args) {
+
+}
+
+func trigProcess(args cli.Args) {
+
 }
