4a8b7b22239b6759a4b6a3c2ef6def92f9fa2779
James Willebrand <99846241+PotatoHead007@users.noreply.github.com>
2022-03-24 01:36:52 +0000

    worked on input checking and Regex stuff

--- something.go
+++ something.go
@@ -6,16 +6,45 @@ import (
     "os"
     "bufio"
     "strings"
+    "sort"
 
     "github.com/urfave/cli/v2"
     regexp "github.com/dlclark/regexp2"
 )
 
+
+const DEBUG = true
+
+
+// function for printing debug info if DEBUG is true.
+func printDebug(v ...interface{}) {
+    if DEBUG {
+        fmt.Println(v...)
+    }
+}
+func printfDebug(format string, v ...interface{}) {
+    if DEBUG {
+        fmt.Printf(format, v...)
+    }
+}
+
 var modes = []string{
-    "1: Basic Arithmetics",
-    "2: Comparisons",
-    "3: Fractions, Decimals, and Percentages",
-    "4: Trigonometry",
+    "\033[1;34m1:\033[0m Basic Arithmetics",
+    "\033[1;34m2:\033[0m Comparisons",
+    "\033[1;34m3:\033[0m Fractions, Decimals, and Percentages",
+    "\033[1;34m4:\033[0m Trigonometry",
+}
+
+// custom type for type sort.Interface
+type captureSort []regexp.Capture
+func (capt captureSort) Len() int {
+    return len(capt)
+}
+func (capt captureSort) Swap(one, two int) {
+    capt[one], capt[two] = capt[two], capt[one]
+}
+func (capt captureSort) Less(one, two int) bool {
+    return capt[one].Index < capt[two].Index
 }
 
 func main() {
@@ -23,7 +52,7 @@ func main() {
         Name: "cli-calculator",
         Description: "A CLI Calculator.",
         Action: func(c *cli.Context) error {
-            fmt.Println("Welcome to CLI-Calculator.")
+            fmt.Println("\033[1;34mWelcome to CLI-Calculator.\033[0m")
             fmt.Println("Please select a mode to continue:")
             for _, mode := range modes {
                 fmt.Println(mode)
@@ -62,13 +91,14 @@ func main() {
         },
         Commands: []*cli.Command{
 			{
-				Name:    "arithmics",
+				Name:    "arithmetic",
 				Aliases: []string{"a", "1"},
-				Usage:   "Quickly compute arithmic problems without using the interactive mode.",
+				Usage:   "Quickly compute arithmetic problems without using the interactive mode.",
 				Action: func(c *cli.Context) error {
 					arithProcess(c.Args(), nil)
 					return nil
 				},
+                SkipFlagParsing: true,
 			},
 			{
 				Name:    "comparisons",
@@ -78,6 +108,7 @@ func main() {
 					compProcess(c.Args())
 					return nil
 				},
+                SkipFlagParsing: true,
 			},
 			{
 				Name:    "fdp",
@@ -87,6 +118,7 @@ func main() {
 					fdpProcess(c.Args())
 					return nil
 				},
+                SkipFlagParsing: true,
 			},
 			{
 				Name:    "trigonometry",
@@ -96,6 +128,7 @@ func main() {
 					trigProcess(c.Args())
 					return nil
 				},
+                SkipFlagParsing: true,
 			},
 		},
     }
@@ -106,6 +139,9 @@ func main() {
     }
 }
 
+// function for processing arithmetic mode input
+// quickArgs is for input from command line arguments,
+// and interactiveArgs is for input in interactive mode.
 func arithProcess(quickArgs cli.Args, interactiveArgs []string) {
     var input string
     if quickArgs == nil && interactiveArgs == nil {
@@ -117,10 +153,9 @@ func arithProcess(quickArgs cli.Args, interactiveArgs []string) {
     } else {
         input = quickArgs.Get(0)
     }
-    fmt.Println(input)
+    printDebug(input)
     
-    fmt.Println("numbers beep boop")
-    var arithRegexVerify = regexp.MustCompile(`(?<!.|\n)(\d+) ?((?:[\+\-]|(?:[\*][\*]?))|(?:[\/][\/]?)) ?(\d+)(?: ?((?:[\+\-]|(?:[\*][\*]?))|(?:[\/][\/]?)) ?(\d+))*(?!.|\n)`, 0)
+    var arithRegexVerify = regexp.MustCompile(`(?<!.|\n)(\(*)*(-?\d+)(\)*)* ?((?:[\+\-]|(?:[\*][\*]?))|(?:[\/][\/]?)) ?(\(*)*(-?\d+)(\)*)*(?: ?((?:[\+\-]|(?:[\*][\*]?))|(?:[\/][\/]?)) ?(\(*)*(-?\d+)(\)*)*)*(?!.|\n)`, 0)
     var match, err = arithRegexVerify.FindStringMatch(input)
     if match == nil {
         log.Fatalln("Invalid input in arithmetic module: input did not match defined regexp.")
@@ -129,26 +164,42 @@ func arithProcess(quickArgs cli.Args, interactiveArgs []string) {
     if err != nil {
         log.Fatal(err)
     }
-    for _, group := range matchGroupCaptues {
-        for _, capture := range group.Captures {
-            fmt.Printf("%v is '%s'\n", capture.Index, capture.String())
+
+    var validCaptures []regexp.Capture
+    for gIdx, group := range matchGroupCaptues {
+        for Idx, capture := range group.Captures {
+            printfDebug("%v is '%s' (%v|%v)\n", capture.Index, capture.String(), gIdx, Idx)
+            if gIdx == 0 && Idx == 0 {
+                printDebug("^ not appended (first capture)")
+                continue
+            } else if capture.Length <= 0 {
+                printDebug("^ not appended (length is 0)")
+            } else {
+                validCaptures = append(validCaptures, capture)
+            }
         }
     }
+    printDebug("\n")
+    for _, capt := range validCaptures {
+        printfDebug("%s : %v : %v\n", capt.String(), capt.Index, capt.Length)
+    }
+    sort.Sort(captureSort(validCaptures))
+    printDebug("\n")
+    for _, capt := range validCaptures {
+        printfDebug("%s : %v : %v\n", capt.String(), capt.Index, capt.Length)
+    }
     
-    fmt.Println("something")
-    /*log.Println(isMatch)
-    log.Println(interactiveArgs[0])*/
-    fmt.Println(input)
+    printDebug(input)
 }
 
 func compProcess(args cli.Args) {
-    fmt.Println("comparing beep boop")
+    fmt.Println("incomplete")
 } 
 
 func fdpProcess(args cli.Args) {
-    fmt.Println("fractions beep boop")
+    fmt.Println("incomplete")
 }
 
 func trigProcess(args cli.Args) {
-    fmt.Println("triangles beep boop")
+    fmt.Println("incomplete")
 }
